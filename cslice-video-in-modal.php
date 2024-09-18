083a8de13ab3a3f941c7559f5e86f3f33da26b35
Tim Bowen <tim@creativeslice.com>
2024-09-18 14:37:09 -0700

    update: check for 'open-video-in-modal' class and use php for performance #8689nv6em

--- cslice-video-in-modal.php
+++ cslice-video-in-modal.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Plugin Name:       Creative Slice Video In Modal
- * Description:       Open video in modal.
+ * Description:       Play YouTube or Vimeo video in modal.
  * Requires at least: 6.4
  * Tested up to:      6.6.2
  * Requires PHP:      8.0
- * Version:           0.1.0
+ * Version:           0.2.0
  * Author:            Creative Slice
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
@@ -46,9 +46,65 @@ function cslice_video_in_modal_enqueue_assets() {
 add_action('wp_enqueue_scripts', 'cslice_video_in_modal_enqueue_assets');
 
 
+
 /**
- * Enqueue files if class exists on button
+ * Add video data attribute to the button block
+ *
+ * @param string $block_content The original HTML content of the block.
+ * @param array  $block         The block details, including attributes.
+ * @return string               The modified block content with the data attribute applied, or the original content if not applicable.
  */
+function cslice_video_in_modal_render_block_core_button($block_content, $block) {
+    // Check if the block has the class 'open-video-in-modal'
+    if (!isset($block['attrs']['className']) || strpos($block['attrs']['className'], 'open-video-in-modal') === false) {
+        return $block_content;
+    }
+
+    // Enqueue files
+    wp_enqueue_style('cslice-video-in-modal-styles');
+    wp_enqueue_script('cslice-video-in-modal-scripts');
+
+    // Modify the button attributes using the HTML API
+    $processor = new WP_HTML_Tag_Processor($block_content);
+
+    if ($processor->next_tag('a')) {
+        $url = $processor->get_attribute('href');
+        if ($url) {
+
+            // Vimeo URL parameters
+			// https://vimeo.com/925983356
+            if (strpos($url, 'vimeo.com') !== false) {
+                $video_id = substr(parse_url($url, PHP_URL_PATH), 1);
+                $url = "https://player.vimeo.com/video/$video_id?autoplay=1";
+
+			// YouTube URL parameters
+			// https://youtu.be/peJbhfeS6Zc
+			// https://www.youtube.com/watch?v=peJbhfeS6Zc
+            } elseif (strpos($url, 'youtube.com') !== false || strpos($url, 'youtu.be') !== false) {
+                $video_id = '';
+                if (strpos($url, 'youtube.com') !== false) {
+                    parse_str(parse_url($url, PHP_URL_QUERY), $query_params);
+                    $video_id = $query_params['v'] ?? '';
+                } elseif (strpos($url, 'youtu.be') !== false) {
+                    $video_id = substr(parse_url($url, PHP_URL_PATH), 1);
+                }
+                if ($video_id) {
+                    $url = "https://www.youtube.com/embed/$video_id?modestbranding=1&autoplay=1&rel=0";
+                }
+            }
+            $processor->set_attribute('data-iframe', esc_url($url));
+        }
+    }
+
+    return $processor->get_updated_html();
+}
+add_filter('render_block_core/button', 'cslice_video_in_modal_render_block_core_button', 10, 2);
+
+
+/**
+ * Enqueue files if 'youtube' or 'vimeo' is in the block content - PERFORMANCE ISSUES
+ */
+/*
 function cslice_video_in_modal_render_block_core_button( $block_content, $block ) {
 	if ( strpos($block_content, 'youtube') !== false || strpos($block_content, 'vimeo') !== false ) {
 		// Enqueue files
@@ -57,7 +113,7 @@ function cslice_video_in_modal_render_block_core_button( $block_content, $block 
 
 		// Add class for js
 		$block_content = new WP_HTML_Tag_Processor( $block_content );
-		$block_content->next_tag(); /* first tag should always be ul or ol */
+		$block_content->next_tag();
 		$block_content->add_class( 'open-video-in-modal' );
 		$block_content->get_updated_html();
 	}
@@ -65,3 +121,4 @@ function cslice_video_in_modal_render_block_core_button( $block_content, $block 
 	return $block_content;
 }
 add_action( 'render_block_core/button', 'cslice_video_in_modal_render_block_core_button', 10, 2 );
+*/
--- src/cslice-video-in-modal.js
+++ src/cslice-video-in-modal.js
@@ -1,77 +1,70 @@
 /**
- * Play Vimeo or YouTube videos from posts in dialog modal
+ * Play Vimeo or YouTube videos in dialog modal
  *
- * @version     2023.12.1
+ * @version     2024.9.18
  * @name        CSlice\JS\Play_Video_In_Dialog
  */
 
-window.addEventListener( 'DOMContentLoaded', function () {
-	/**
-	 * Create dialog element & close button (used below)
-	 */
-	function createDialog() {
-		const dialog = document.createElement( 'dialog' );
-		dialog.className = 'dialog-modal-video';
+window.addEventListener('DOMContentLoaded', function () {
+    /**
+     * Create dialog element & close button (used below)
+     */
+    function createDialog() {
+        const dialog = document.createElement('dialog');
+        dialog.className = 'dialog-modal-video';
+        dialog.setAttribute('aria-label', '');
+        dialog.setAttribute('role', 'dialog');
+        dialog.setAttribute('aria-modal', 'true');
+        dialog.setAttribute('tabindex', '-1');
 
-		const closeButton = document.createElement( 'button' );
-		closeButton.className = 'dialog-close';
-		closeButton.innerHTML = '&#215;';
-		closeButton.addEventListener( 'click', () => {
-			dialog.close();
-			dialog.classList.add( 'dialog-closed' );
-		} );
-		dialog.appendChild( closeButton );
+        const closeButton = document.createElement('button');
+        closeButton.className = 'dialog-close';
+        closeButton.setAttribute('aria-label', 'Close');
+        closeButton.addEventListener('click', () => {
+            dialog.close();
+        });
+        dialog.appendChild(closeButton);
 
-		return dialog;
-	}
+        const content = document.createElement('div');
+        content.className = 'dialog-content';
+        dialog.appendChild(content);
 
-	/**
-	 * Play video in modal
-	 */
-	const videoModals = document.querySelectorAll( '.open-video-in-modal' );
-	if ( videoModals ) {
-		videoModals.forEach( ( videoModal ) => {
-			const videoModalButton = videoModal;
-			const link = videoModalButton.querySelector('a');
-			videoModalButton.addEventListener( 'click', ( event ) => {
-				event.preventDefault();
+        // Listen for the close event to remove the dialog and stop the video
+        dialog.addEventListener('close', () => {
+            dialog.remove();
+        });
 
-				const videoModalContent = document.createElement( 'div' );
-				videoModalContent.className = 'dialog-content';
+        return { dialog, content };
+    }
 
-				// Vimeo & YouTube URL parameters
-				let videoUrl = link.href;
-				console.log('videoUrl', videoUrl);
-				if ( videoUrl && videoUrl.includes( 'vimeo.com' ) ) {
-					const videoId = videoUrl.split( '/' ).pop();
-					videoUrl = `https://player.vimeo.com/video/${ videoId }?autoplay=1`;
-				}
-				if ( videoUrl && videoUrl.includes( 'youtube.com' ) ) {
-					const videoId = new URL( videoUrl ).searchParams.get( 'v' );
-					videoUrl = `https://www.youtube.com/embed/${ videoId }?modestbranding=1&autoplay=1&rel=0`;
-				}
+    /**
+     * Play video in modal
+     */
+    const videoModals = document.querySelectorAll('.open-video-in-modal');
+    if (videoModals) {
+        videoModals.forEach((videoModal) => {
+            const videoModalButton = videoModal;
+            const link = videoModalButton.querySelector('a');
+            videoModalButton.addEventListener('click', (event) => {
+                event.preventDefault();
 
-				const videoEmbed = document.createElement( 'iframe' );
-				videoEmbed.src = videoUrl;
-				videoEmbed.frameborder = '0';
-				videoEmbed.allow =
-					'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture';
-				videoEmbed.allowFullscreen = true;
+                const { dialog, content } = createDialog();
 
-				videoModalContent.appendChild( videoEmbed );
+                // Use the data-iframe attribute for the video URL
+                const videoUrl = link.getAttribute('data-iframe');
 
-				const dialog = createDialog();
-				dialog.appendChild( videoModalContent );
-				document.body.appendChild( dialog );
+                const videoEmbed = document.createElement('iframe');
+                videoEmbed.src = videoUrl;
+                videoEmbed.frameborder = '0';
+                videoEmbed.allow =
+                    'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture';
+                videoEmbed.allowFullscreen = true;
 
-				dialog.showModal();
-				videoEmbed.focus();
+                content.appendChild(videoEmbed);
 
-				// Close dialog on click outside
-				dialog.addEventListener( 'click', () => {
-					dialog.remove();
-				} );
-			} );
-		} );
-	}
-} );
+                document.body.appendChild(dialog);
+                dialog.showModal();
+            });
+        });
+    }
+});
